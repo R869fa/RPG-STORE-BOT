e423a08adb21693621ccd1fec90880a51034e36a
Rafael <rafaeluchihaotsutsuki@gmail.com>
2024-04-03 00:59:30 -0300

    v1

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+@bot.command()
+async def loja(ctx):
+    await ctx.send("Bem-vindo à loja! Itens disponíveis:\n1. Espada - 100 moedas\n2. Poção de Cura - 50 moedas")
+
+@bot.command()
+async def comprar(ctx, item_id: int):
+    if item_id == 1:
+        await ctx.send("Você comprou uma Espada!")
+    elif item_id == 2:
+        await ctx.send("Você comprou uma Poção de Cura!")
+    else:
+        await ctx.send("Item inválido!")
+
+bot.run('MTIyNDkyMTExMjkyNDc4NjcwOA.GMRwXK.HaZ-oMczCPfCbJm1LsunsOLX1sOao8FodnCBSc')
