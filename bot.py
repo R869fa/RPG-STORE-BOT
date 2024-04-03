486b13f9001b795571efd58be167fa213451f561
Rafael <rafaeluchihaotsutsuki@gmail.com>
2024-04-03 01:26:14 -0300

    corrigido falta de argumento de incilização

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.members = True
+
 bot = commands.Bot(command_prefix="!")
 @bot.command()
 async def loja(ctx):
