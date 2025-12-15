aa9493f6c2d225e679d7b2b7134a1ea36d861fd6
Rafael <74325040+R869fa@users.noreply.github.com>
2025-12-15 16:37:27 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,22 @@
-import discord 
+import discord
 from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+
+async def processar_transação(ctx, item_id: int, acao: str):
+    Item = {
+    1: "Espada",
+    2: "Poção de Cura"
+}       
+    Item = Item.get (item_id)
+    if not Item:
+        await ctx.send("Item Inválido!")
+        return
+    
+    await ctx.send(f"Você {acao} uma {Item}!")
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.command()
@@ -13,13 +25,13 @@ async def loja(ctx):
 
 @bot.command()
 async def comprar(ctx, item_id: int):
-    if item_id == 1:
-        await ctx.send("Você comprou uma Espada!")
-    elif item_id == 2:
-        await ctx.send("Você comprou uma Poção de Cura!")
-    else:
-        await ctx.send("Item inválido!")
-        
+        await processar_transação(ctx, item_id, "comprou")
+        await ctx.send()
+
+@bot.command()
+async def vender(ctx, item_id: int):
+        await processar_transação(ctx, item_id, "vendeu")
+
 @bot.event
 async def on_ready():
     print(f"Conectado como {bot.user}")
